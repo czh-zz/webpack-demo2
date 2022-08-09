592d110fd9fd7f8710fe65fe5546b58ec42ceb5e
chenzh <515740090@qq.com>
2022-08-09 22:31:46 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -7,5 +9,17 @@ module.exports = {
         //filename: 'index.js',
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'webpack首页',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
 };
