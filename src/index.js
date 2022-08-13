52674c5bb6bc99c2325a37d1f5563a29bd96367d
chenzh <515740090@qq.com>
2022-08-13 17:43:50 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import x from './x.js'
 import png from './assets/cat.png'
 
-const div = document.getElementById('app')
-div.innerHTML = `
-    <img src="${png}">
+// const div = document.getElementById('app')
+// div.innerHTML = `
+//     <img src="${png}">
 `
 const button = document.createElement('button')
 button.innerText = '懒加载'
